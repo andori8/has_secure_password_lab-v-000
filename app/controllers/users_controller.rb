d0487254a6266db65027bf22777275beebafe5ef
Ryan Do <falcsmike7@gmail.com>
2017-10-10 22:14:22 +0000

    automatically backed up by learn

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,12 @@ class UsersController < ApplicationController
     @user = User.new
   end
 
+  def create
+    @user = User.new(user_params)
+      if @user.save
+        redirect_to 
+  end
+
   private
 
   def user_params

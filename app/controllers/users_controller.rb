5f785514a122023fee9d926223ce17066d71bf38
Ryan Do <falcsmike7@gmail.com>
2017-10-10 22:49:32 +0000

    automatically backed up by learn

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,7 +6,7 @@ class UsersController < ApplicationController
   def create
     user = User.new(user_params)
       if user.authenticate(params[:user][:password])
-        session[:user_id] = @user.id
+        session[:user_id] = user.id
         redirect_to user_path
       else
         redirect_to new_user_path
